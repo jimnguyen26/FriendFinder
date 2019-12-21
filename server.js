76809bf958d650c270ab6548f9f243828f769f9f
Jim <jimmybnguyen@yahoo.com>
2019-12-20 17:45:18 -0800

    req,res working

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
-console.log(process.argv);
-
+// console.log(process.argv);
 
 const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./app/public"));
+
+const apiRoutes = require("./app/routing/apiRoutes");
+app.use("/api", apiRoutes);
+
+const htmlRoutes = require("./app/routing/htmlRoutes");
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`listening on local: http://localhost:${PORT}`);
+});
