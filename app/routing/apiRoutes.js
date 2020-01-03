1426137d47c032760a062f75f07d511f70f7dfbd
Jim <jimmybnguyen@yahoo.com>
2020-01-03 12:28:49 -0800

    friend added

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const friends = require("../data/friends");
 
 router.get("/", (req, res) => {
     console.log("hit our server");
     res.send("nice");
 });
+
+
 
 module.exports = router;
